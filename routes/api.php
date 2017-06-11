4f3ff7eb2a7a810a52ed7dba66dc19c6d88f3e7c
The Invisible Man <granados.carlos91@gmail.com>
2017-06-10 21:11:20 -0400

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'streams'], function ()
+{
+    Route::get('/', 'StreamsController@get');
+    Route::get('/{stream_id}', 'StreamsController@all');
+});
